b034dd389ceaf024d28c4a7b38996f76a66b9bda
kojo8888 <kojo88@gmx.net>
2023-01-28 12:55:55 +0100

    Kettenlänge

--- pages/Kettenl.js
+++ pages/Kettenl.js
@@ -1,68 +1,120 @@
-import Head from "next/head";
-import HeaderComponent from "../components/header";
-import FooterComponent from "../components/footer";
-import { Plus } from "react-feather";
+export default function Kettenl() {
+  const submitContact = async (event) => {
+    event.preventDefault();
 
-export default function Kettenlänge({ products }) {
+    //Variablen zum Abfragen
+    const lKettenstrebe = event.target.lKettenstrebe.value;
+    const ZKettenblatt = event.target.ZKettenblatt.value;
+    const ZRitzel = event.target.ZRitzel.value;
+    const Schaltungsroellchen = event.target.Schaltungsroellchen.value;
+
+    // Variablen zum Rechnen
+    let Klungerundet;
+    let Klgerundet;
+    let Klgerade;
+
+    // Ergebnis
+    let Kl;
+
+    //Rechnung
+    if (parseInt(Schaltungsroellchen) === 10) {
+      Klungerundet =
+        0.157 * parseInt(lKettenstrebe) +
+        (1 / 2) * parseInt(ZKettenblatt) +
+        (1 / 2) * parseInt(ZRitzel) +
+        2;
+      Klgerundet = Math.ceil(Klungerundet);
+      if (Klgerundet % 2 != 0) {
+        Klgerade = Klgerundet + 1;
+      } else Klgerade = Klgerundet;
+    } else if (parseInt(Schaltungsroellchen) === 11) {
+      Klungerundet =
+        0.157 * parseInt(lKettenstrebe) +
+        (1 / 2) * parseInt(ZKettenblatt) +
+        (1 / 2) * parseInt(ZRitzel) +
+        2 +
+        2;
+      Klgerundet = Math.ceil(Klungerundet);
+      if (Klgerundet % 2 != 0) {
+        Klgerade = Klgerundet + 1;
+      } else Klgerade = Klgerundet;
+    } else if (parseInt(Schaltungsroellchen) === 12) {
+      Klungerundet =
+        0.157 * parseInt(lKettenstrebe) +
+        (1 / 2) * parseInt(ZKettenblatt) +
+        (1 / 2) * parseInt(ZRitzel) +
+        2 +
+        4;
+
+      Klgerundet = Math.ceil(Klungerundet);
+      if (Klgerundet % 2 != 0) {
+        Klgerade = Klgerundet + 1;
+      } else Klgerade = Klgerundet;
+    }
+    Kl = Klgerade;
+    alert(`Deine Kettenlänge ist ${Kl}?`);
+  };
+
   return (
-    <div className="font-mono mt-10 mx-auto text-center max-w-7xl px-10">
-      <HeaderComponent></HeaderComponent>
-      <form
-        action="/test"
-        method="POST"
-        className="w-1/2 mx-auto mt-10 text-center"
-      >
-        <label className="block mb-3" for="lKettenstrebe">
-          Kettenstrebenlänge in mm
-        </label>
-        <input
-          className="w-half p-2 mb-3 border border-gray-400 border-solid rounded-lg"
-          type="number"
-          name="lKettenstrebe"
-          id="lKettenstrebe"
-          placeholder="gewöhnlich ~420mm"
-        />
-        <label className="block mb-3" for="ZKettenblatt">
-          Zähnezahl größtes Kettenblatt (vorne)
-        </label>
-        <input
-          className="w-half p-2 mb-3 border border-gray-400 border-solid rounded-lg"
-          type="number"
-          name="ZKettenblatt"
-          id="ZKettenblatt"
-          placeholder="~44"
-        />
-        <label className="block mb-3" for="ZRitzel">
-          Zähnezahl größtes Ritzel (hinten)
-        </label>
-        <input
-          className="w-half p-2 mb-3 border border-gray-400 border-solid rounded-lg"
-          type="number"
-          name="ZRitzel"
-          id="ZRitzel"
-          placeholder="~34"
-        />
-        <label className="block mb-3" for="Schaltungsröllchen">
-          Schaltungsröllchen
-        </label>
-        <select
-          className="w-half p-2 mb-3 border border-gray-400 border-solid rounded-lg"
-          id="Schaltungsröllchen"
-          name="Schaltungsröllchen"
-        >
-          <option value="10">10</option>
-          <option value="11">11</option>
-          <option value="12">12</option>
-        </select>
-        <label> </label>
-        <button
-          type="submit"
-          className="font-medium text-white hover:bg-blue-600 bg-blue-500 px-3 py-2 -mt-2 rounded-lg"
-        >
-          Save
-        </button>
-      </form>
-      <FooterComponent></FooterComponent>
+    <div className="max-w-xs my-2 overflow-hidden rounded shadow-lg">
+      <div className="px-6 py-4">
+        <div className="mb-2 text-xl font-bold">Kettenlänge berechnen</div>
+        <form className="flex flex-col" onSubmit={submitContact}>
+          <label className="block mb-3" htmlfor="lKettenstrebe">
+            Kettenstrebenlänge in mm
+          </label>
+          <input
+            className="w-half p-2 mb-3 border border-gray-400 border-solid rounded-lg"
+            type="number"
+            name="lKettenstrebe"
+            id="lKettenstrebe"
+            placeholder="gewöhnlich ~420mm"
+            required
+          />
+          <label className="block mb-3" htmlfor="ZKettenblatt">
+            Zähnezahl größtes Kettenblatt (vorne)
+          </label>
+          <input
+            className="w-half p-2 mb-3 border border-gray-400 border-solid rounded-lg"
+            type="number"
+            name="ZKettenblatt"
+            id="ZKettenblatt"
+            placeholder="~44"
+            required
+          />
+          <label className="block mb-3" htmlfor="ZRitzel">
+            Zähnezahl größtes Ritzel (hinten)
+          </label>
+          <input
+            className="w-half p-2 mb-3 border border-gray-400 border-solid rounded-lg"
+            type="number"
+            name="ZRitzel"
+            id="ZRitzel"
+            placeholder="~34"
+            required
+          />
+          <label className="block mb-3" htmlfor="Schaltungsröllchen">
+            Schaltungsröllchen
+          </label>
+          <select
+            className="w-half p-2 mb-3 border border-gray-400 border-solid rounded-lg"
+            id="Schaltungsroellchen"
+            name="Schaltungsroellchen"
+            required
+          >
+            <option value="10">10</option>
+            <option value="11">11</option>
+            <option value="12">12</option>
+          </select>
+
+          <button
+            type="submit"
+            className="px-4 py-2 font-bold text-white bg-blue-500 rounded-full hover:bg-blue-700"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
