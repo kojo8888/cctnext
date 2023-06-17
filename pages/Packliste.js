a898a0a65151b067d9d3c9573d56ec4a22125941
kojo8888 <kojo88@gmx.net>
2023-06-17 22:01:13 +0200

    Packliste

--- pages/Packliste.js
+++ pages/Packliste.js
@@ -1,13 +1,68 @@
-import Table from "../components/Table";
-import data from "../data.json";
+import { useState, useEffect } from "react";
 import HeaderComponent from "../components/header";
 import FooterComponent from "../components/footer";
+import { Plus } from "react-feather";
 import Head from "next/head";
 
-function Packliste() {
-  const getHeadings = () => {
-    return Object.keys(data[0]);
-  };
+export default function ExampleCheckbox() {
+  const [showBasis, setshowBasis] = useState();
+  const [showNass, setshowNass] = useState();
+  const [showWplatz, setshowWplatz] = useState();
+  const [showVplatz, setshowVplatz] = useState();
+  const apiUrl = "/api/liste";
+  let displayData;
+  let dData;
+
+  function pullJson() {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((responseData) => {
+        displayData = responseData
+          .filter((el) => el.category == "Werkzeug")
+          .map(function (liste) {
+            return <p key={liste.id}>{liste.name}</p>;
+          });
+
+        setshowBasis(displayData);
+      });
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((rData) => {
+        dData = rData
+          .filter((els) => els.category == "Elektronik")
+          .map(function (liste) {
+            return <p key={liste.id}>{liste.name}</p>;
+          });
+
+        setshowNass(dData);
+      });
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((rData) => {
+        dData = rData
+          .filter((els) => els.category == "Elektronik")
+          .map(function (liste) {
+            return <p key={liste.id}>{liste.name}</p>;
+          });
+
+        setshowVplatz(dData);
+      });
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((rData) => {
+        dData = rData
+          .filter((els) => els.category == "Elektronik")
+          .map(function (liste) {
+            return <p key={liste.id}>{liste.name}</p>;
+          });
+
+        setshowWplatz(dData);
+      });
+  }
+
+  useEffect(() => {
+    pullJson();
+  }, []);
 
   return (
     <div className="font-mono mt-10 mx-auto text-center max-w-7xl px-10">
@@ -15,11 +70,67 @@ function Packliste() {
         <title>Packliste</title>
       </Head>
       <HeaderComponent></HeaderComponent>
-      <div className="container">
-        <Table theadData={getHeadings()} tbodyData={data} />
+
+      <div className="space-y-12 sm:space-y-0 sm:grid sm:grid-cols-4 sm:gap-x-3 sm:gap-y-3">
+        <div className="p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+          <h3 className="text-3xl font-semibold text-gray-900">Basis</h3>
+          <p className="mt-4 mx-auto"></p>
+          <ul role="list" className="mt-6 space-y-6">
+            <li className="flex">
+              <span className="ml-3 text-left">{showBasis}</span>
+            </li>
+          </ul>
+        </div>
+        <div className="p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+          <h3 className="text-3xl font-semibold text-gray-900">
+            Nass und kalt
+          </h3>
+          <p className="mt-4 mx-auto"></p>
+          <ul role="list" className="mt-6 space-y-6">
+            <li className="flex">
+              <span className="text-blue-500 text-3xl">
+                <Plus />
+              </span>
+              <span className="ml-3 text-black text-left">{showNass}</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+          <h3 className="text-3xl font-semibold text-gray-900">Wenig Platz</h3>
+          <ul role="list" className="mt-6 space-y-6">
+            <li className="flex">
+              <span className="text-blue-500 text-3xl">
+                <Plus />
+              </span>
+              <span className="ml-3 text-black text-left">{showWplatz}</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
+          <h3 className="text-3xl font-semibold text-gray-900">Viel Platz</h3>
+          <ul role="list" className="mt-6 space-y-6">
+            <li className="flex">
+              <span className="text-blue-500 text-3xl">
+                <Plus />
+              </span>
+              <span className="ml-3 text-black text-left">{showVplatz}</span>
+            </li>
+          </ul>
+        </div>
       </div>
+
+      <div className="p-8">
+        <a
+          href="mailto:customcyclingtracks@gmx.net"
+          className="font-medium text-white hover:bg-blue-600 bg-blue-500 px-3 py-2 -mt-2 rounded-lg"
+        >
+          Schreib uns
+        </a>
+      </div>
+
       <FooterComponent></FooterComponent>
     </div>
   );
 }
-export default Packliste;
---
+++ pages/Packlistetabelle.js
@@ -0,0 +1,25 @@
+import Table from "../components/Table";
+import data from "../data.json";
+import HeaderComponent from "../components/header";
+import FooterComponent from "../components/footer";
+import Head from "next/head";
+
+function Packliste() {
+  const getHeadings = () => {
+    return Object.keys(data[0]);
+  };
+
+  return (
+    <div className="font-mono mt-10 mx-auto text-center max-w-7xl px-10">
+      <Head>
+        <title>Packliste</title>
+      </Head>
+      <HeaderComponent></HeaderComponent>
+      <div className="container">
+        <Table theadData={getHeadings()} tbodyData={data} />
+      </div>
+      <FooterComponent></FooterComponent>
+    </div>
+  );
+}
+export default Packliste;
