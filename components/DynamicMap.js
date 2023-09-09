b3e6366158f310541a9e2fa9f5c807250fd9a437
kojo8888 <kojo88@gmx.net>
2023-09-09 18:37:35 +0200

    Prisma production, geojson display

--- components/DynamicMap.js
+++ components/DynamicMap.js
@@ -1,16 +1,38 @@
 import React, { useState } from "react";
-import { MapContainer, TileLayer, Marker, Popup, Circle } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Circle,
+  GeoJSON,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import { useMapEvent } from "react-leaflet/hooks";
+import L from "leaflet";
+import Kreis from "../lib/Kreis.json";
 
 const DynamicMap = () => {
   const [circleCenter, setCircleCenter] = useState(null);
   const [circleRadius, setCircleRadius] = useState(10000); // Default radius in meters
 
-  function MapContent({ customIcon }) {
+  // Load and display GeoJSON data
+  // fetch("../lib/Kreis.geojson")
+  //   .then((response) => response.json())
+  //   .then((geojson) => {
+  //     L.geoJSON(geojson).addTo(map);
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error loading GeoJSON:", error);
+  //   });
+
+  const setColor = ({ properties }) => {
+    return { weight: 1 };
+  };
+
+  function MapContent({ map }) {
     const [markerPosition, setMarkerPosition] = useState(null);
 
-    const map = useMapEvent("click", (e) => {
+    useMapEvent("click", (e) => {
       const { lat, lng } = e.latlng;
       console.log("Clicked at:", lat, lng);
       setMarkerPosition([lat, lng]);
@@ -27,11 +49,12 @@ const DynamicMap = () => {
         style={{ width: "100%", height: "500px" }}
       >
         <TileLayer
-          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          url="https://{s}.tile-cyclosm.openstreetmap.fr/cyclosm/{z}/{x}/{y}.png"
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         />
-
+        <GeoJSON data={Kreis} style={setColor} />
         <MapContent />
+        {circleCenter && <Circle center={circleCenter} radius={circleRadius} />}
       </MapContainer>
       <div>
         <input
@@ -40,11 +63,10 @@ const DynamicMap = () => {
           max="200000"
           step="1000"
           value={circleRadius}
-          onChange={(e) => setCircleRadius(e.target.value)}
+          onChange={(e) => setCircleRadius(parseInt(e.target.value))}
         />
         <p>Circle Radius: {circleRadius} meters</p>
       </div>
-      <div id="map"></div>
     </div>
   );
 };
