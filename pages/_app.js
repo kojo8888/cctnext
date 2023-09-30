7254ec4eb5cbf802d029473d315ca46a770ef329
kojo8888 <kojo88@gmx.net>
2023-09-30 21:06:45 +0200

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,19 @@
 import "../styles/globals.css";
-// import Layout from "../components/layout";
+import Layout from "../components/layout";
 import Script from "next/script";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Component {...pageProps} />
-      <Script
-        strategy="lazyOnload"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-      />
+      <Layout>
+        <Component {...pageProps} />
+        <Script
+          strategy="lazyOnload"
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
 
-      <Script strategy="lazyOnload">
-        {`
+        <Script strategy="lazyOnload">
+          {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -20,8 +21,9 @@ export default function App({ Component, pageProps }) {
                     page_path: window.location.pathname,
                     });
                 `}
-      </Script>
-      {/* <Analytics /> */}
+        </Script>
+        {/* <Analytics /> */}
+      </Layout>
     </>
   );
 }
