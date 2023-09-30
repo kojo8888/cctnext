dc51d4d327c61754f8d20483ecdeb8fb7549a90f
kojo8888 <kojo88@gmx.net>
2023-09-30 20:42:38 +0200

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,18 @@
 import "../styles/globals.css";
-import { Analytics } from "@vercel/analytics/react";
-import Layout from "../components/layout";
+// import Layout from "../components/layout";
 import Script from "next/script";
-import Router from "next/router";
 
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Layout>
-        <Component {...pageProps} />
-        <Script
-          strategy="lazyOnload"
-          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
-        />
+      <Component {...pageProps} />
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
 
-        <Script strategy="lazyOnload">
-          {`
+      <Script strategy="lazyOnload">
+        {`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
@@ -23,9 +20,8 @@ export default function App({ Component, pageProps }) {
                     page_path: window.location.pathname,
                     });
                 `}
-        </Script>
-        {/* <Analytics /> */}
-      </Layout>
+      </Script>
+      {/* <Analytics /> */}
     </>
   );
 }
