0846549002ad5fec3fa5a87b598396cc14fe79e9
kojo8888 <kojo88@gmx.net>
2023-11-11 16:09:04 +0100

    Update Packlistetest

--- components/DataDisplay.js
+++ components/DataDisplay.js
@@ -11,20 +11,52 @@ const DataDisplay = ({ checkboxState }) => {
       const responseData = await response.json();
 
       if (checkboxState.checkbox1) {
-        const allData = responseData.map((item) => (
-          <p key={item.id}>
-            <a href={item.link}>{item.name}</a>
-          </p>
-        ));
+        //Nass und kalt
+        const allData = responseData
+          .filter((els) => els.location == "kalt")
+          .map(function (liste) {
+            return <p key={liste.id}>{liste.name}</p>;
+          });
+
+        setApiData(allData);
+      }
+      if (checkboxState.checkbox2) {
+        //Werkzeug
+        const allData = responseData
+          .filter((els) => els.category == "Werkzeug")
+          .map(function (liste) {
+            return <p key={liste.id}>{liste.name}</p>;
+          });
+
+        setApiData(allData);
+      }
+      if (checkboxState.checkbox3) {
+        //Elektronik
+        const allData = responseData
+          .filter((els) => els.category == "Elektronik")
+          .map(function (liste) {
+            return <p key={liste.id}>{liste.name}</p>;
+          });
+
+        setApiData(allData);
+      }
+      if (checkboxState.checkbox4) {
+        //Ersatzteile
+        const allData = responseData
+          .filter((els) => els.category == "Ersatzteile")
+          .map(function (liste) {
+            return <p key={liste.id}>{liste.name}</p>;
+          });
 
         setApiData(allData);
       } else {
-        // Reset data if checkbox1 is not checked
-        const allData = responseData.map((item) => (
-          <p key={item.id}>{item.name}</p>
-        ));
-
-        setApiData(allData);
+        // // Reset data if checkbox1 is not checked
+        // const allData = responseData.map((item) => (
+        //   <p key={item.id}>
+        //     <a href={item.url}>{item.name}</a>
+        //   </p>
+        // ));
+        // setApiData(allData);
       }
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -34,7 +66,7 @@ const DataDisplay = ({ checkboxState }) => {
 
   useEffect(() => {
     fetchData();
-  }, [checkboxState.checkbox1]);
+  }, [checkboxState]);
 
   return (
     <div className="space-y-12">
