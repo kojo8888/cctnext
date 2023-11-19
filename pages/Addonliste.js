021b7dbb5a3f867a265890e835ace61553cd2fb7
kojo8888 <kojo88@gmx.net>
2023-11-19 14:15:21 +0100

    Filter Addon liste

--- pages/Addonliste.js
+++ pages/Addonliste.js
@@ -1,57 +1,89 @@
 import { useState, useEffect } from "react";
-import { Plus } from "react-feather";
 import Head from "next/head";
 
 export default function ExampleCheckbox() {
-  const [showWerkzeug, setshowWerkzeug] = useState();
-
-  // const [showVplatz, setshowVplatz] = useState();
+  const [allData, setAllData] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState(null); // State for selected category
   const apiUrl = "/api/Strava";
-  let allData;
 
-  function pullJson() {
+  // Fetch the data and update the state
+  useEffect(() => {
     fetch(apiUrl)
       .then((response) => response.json())
       .then((responseData) => {
-        allData = responseData.map(function (liste) {
-          return (
-            <p key={liste.id}>
-              <a href={liste.link}>{liste.name}</a>
-            </p>
-          );
-        });
-        setshowWerkzeug(allData);
+        const formattedData = responseData.map((item) => ({
+          id: item.id,
+          name: item.name,
+          link: item.link,
+          rating: item.rating,
+          category: item.category,
+          subcategory: item.subcategory,
+        }));
+        setAllData(formattedData);
       });
-  }
+  }, [apiUrl]);
 
-  useEffect(() => {
-    pullJson();
-  }, []);
+  // Filter data based on selected category
+  const filteredData = selectedCategory
+    ? allData.filter((item) => item.category === selectedCategory)
+    : allData;
 
   return (
     <div className="font-mono mt-10 mx-auto text-center max-w-7xl px-10">
-      <Head>
-        <title>Strava Add-on Liste</title>
-        <meta name="description" content="Strava Add-on Liste" key="desc" />
-        <meta property="og:title" content="Strava Add-on Liste" />
-        <meta property="og:description" content="Strava Add-on Liste" />
-        <meta
-          property="og:image"
-          content="https://www.customcyclingtracks.com/Logo.png"
-        />
-      </Head>
-      {/* <HeaderComponent></HeaderComponent> */}
+      <Head>{/* ... (your existing Head content) */}</Head>
 
       <div className="space-y-12">
         <div className="p-8 bg-white border border-gray-200 rounded-2xl shadow-sm flex flex-col">
           <h3 className="text-3xl font-semibold text-gray-900">
             Strava Add-ons
           </h3>
-          <ul role="list" className="mt-6 space-y-6">
-            <li className="flex">
-              <span className="ml-3 text-left">{showWerkzeug}</span>
-            </li>
-          </ul>
+
+          {/* Filter button */}
+          <div className="mb-4 bg-red-100">
+            <button
+              onClick={() => setSelectedCategory(null)}
+              className={`mr-2 ${
+                selectedCategory === null && "bg-red-300 text-gray-500"
+              }`}
+            >
+              All
+            </button>
+            {Array.from(new Set(allData.map((item) => item.category))).map(
+              (category) => (
+                <button
+                  key={category}
+                  onClick={() => setSelectedCategory(category)}
+                  className={`mr-2 ${
+                    selectedCategory === category && "bg-red-300 text-gray-500"
+                  }`}
+                >
+                  {category}
+                </button>
+              )
+            )}
+          </div>
+
+          {/* Table */}
+          <table className="table-auto">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Kategorie</th>
+              </tr>
+            </thead>
+            <tbody>
+              {filteredData.map((item) => (
+                <tr key={item.id}>
+                  <td>
+                    <a className="hover:underline" href={item.link}>
+                      {item.name}
+                    </a>
+                  </td>
+                  <td>{item.category}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
         </div>
       </div>
 
@@ -63,8 +95,6 @@ export default function ExampleCheckbox() {
           Schreib uns
         </a>
       </div>
-
-      {/* <FooterComponent></FooterComponent> */}
     </div>
   );
 }
