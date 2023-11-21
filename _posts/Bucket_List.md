f5f694c6d8b9f8be51d6f8b3e9621e1512125d66
kojo8888 <kojo88@gmx.net>
2023-11-21 20:38:30 +0100

    bucket liste

--- _posts/Bucket_List.md
+++ _posts/Bucket_List.md
@@ -1,23 +1,19 @@
 ---
-
 title: "Bucket List: Touren, die jeder Rennradler gemacht haben sollte."
 
-excerpt: "Viele Wege führen nach Rom, doch manche sind schneller. Hier erfahrt ihr, wie ihr schnell zur Rennradroute kommt."
+excerpt: "Touren, die man gemacht haben sollte."
 
 coverImage: "/assets/blog/authors/joe.jpeg"
 
 date: "2023-10-16T05:35:07.322Z"
 
 author:
+  name: Konstantin
 
-name: Konstantin
-
-picture: "/assets/blog/authors/Konstantin.png"
+  picture: "/assets/blog/authors/Konstantin.png"
 
 ogImage:
-
-url: "/assets/blog/authors/joe.jpeg"
-
-—
+  url: "/assets/blog/authors/joe.jpeg"
+---
 
 # Bucket List: Touren, die jeder Rennradler gemacht haben sollte.
--- pages/api/Overpass.js
+++ pages/api/Overpass.js
@@ -5,27 +5,50 @@ const fs = require("fs");
 const overpassQuery = `
   [out:json];
   area["name"="München"]->.searchArea;
-  node["amenity"="drinking_water"](area.searchArea);
+  node["amenity"="fountain"](area.searchArea);
   out body;
 `;
 
 // Define the Overpass API endpoint
 const overpassApiUrl = "https://overpass-api.de/api/interpreter";
 
-// Function to fetch data from Overpass API
+// Function to format Overpass API response to GeoJSON
+function formatToGeoJSON(overpassResponse) {
+  const geojsonFeatures = overpassResponse.elements.map((element) => ({
+    type: "Feature",
+    geometry: {
+      type: "Point",
+      coordinates: [element.lon, element.lat],
+    },
+    properties: {
+      id: element.id,
+      tags: element.tags,
+    },
+  }));
+
+  return {
+    type: "FeatureCollection",
+    features: geojsonFeatures,
+  };
+}
+
+// Function to fetch data from Overpass API and save as GeoJSON
 async function fetchData() {
   try {
     // Make a POST request to Overpass API
     const response = await axios.post(overpassApiUrl, overpassQuery);
 
-    // Extract data from the response
-    const data = response.data;
+    // Format the response to GeoJSON
+    const geojsonData = formatToGeoJSON(response.data);
 
-    // Save the data to a file (you can modify the filename as needed)
-    fs.writeFileSync("fountain.json", JSON.stringify(data, null, 2));
+    // Save the GeoJSON data to a file (you can modify the filename as needed)
+    fs.writeFileSync(
+      "munich_fountains.geojson",
+      JSON.stringify(geojsonData, null, 2)
+    );
 
     console.log(
-      "Data has been successfully fetched and saved to munich_fountains.json"
+      "Data has been successfully fetched and saved to munich_fountains.geojson"
     );
   } catch (error) {
     console.error("Error fetching data from Overpass API:", error.message);
