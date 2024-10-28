2a41910904b988a879afdcffb3ef5a10b42a25a5
kojo8888 <kojo88@gmx.net>
2024-10-28 18:54:07 +0100

    Stripe aktiv

--- pages/api/stripe/checkout.js
+++ pages/api/stripe/checkout.js
@@ -1,37 +1,37 @@
-// import Stripe from "stripe";
+import Stripe from "stripe";
 
-// const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
-// export default async function handler(req, res) {
-//   if (req.method === "POST") {
-//     try {
-//       // Create a checkout session
-//       const session = await stripe.checkout.sessions.create({
-//         payment_method_types: ["card"],
-//         line_items: [
-//           {
-//             price_data: {
-//               currency: "eur",
-//               product_data: {
-//                 name: "Tip Payment",
-//               },
-//               unit_amount: 1000, // Amount in cents (e.g., $10.00)
-//             },
-//             quantity: 1,
-//           },
-//         ],
-//         mode: "payment",
-//         success_url: `${req.headers.origin}/tipsplus?session_id={CHECKOUT_SESSION_ID}`,
-//         cancel_url: `${req.headers.origin}/cancel`,
-//       });
+export default async function handler(req, res) {
+  if (req.method === "POST") {
+    try {
+      // Create a checkout session
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ["card"],
+        line_items: [
+          {
+            price_data: {
+              currency: "eur",
+              product_data: {
+                name: "Tip Payment",
+              },
+              unit_amount: 1000, // Amount in cents (e.g., $10.00)
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        success_url: `${req.headers.origin}/tipsplus?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${req.headers.origin}/cancel`,
+      });
 
-//       // Return the session ID to the client
-//       res.status(200).json({ sessionId: session.id });
-//     } catch (err) {
-//       res.status(500).json({ error: err.message });
-//     }
-//   } else {
-//     res.setHeader("Allow", "POST");
-//     res.status(405).end("Method Not Allowed");
-//   }
-// }
+      // Return the session ID to the client
+      res.status(200).json({ sessionId: session.id });
+    } catch (err) {
+      res.status(500).json({ error: err.message });
+    }
+  } else {
+    res.setHeader("Allow", "POST");
+    res.status(405).end("Method Not Allowed");
+  }
+}
